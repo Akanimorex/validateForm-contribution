cd3a559579dc054d409c4a0d586fdf8aba9817fe
Akanimorex <akanimoime2016@gmail.com>
2020-06-18 15:24:11 +0100

    validation of registration form

---
+++ form.js
@@ -0,0 +1,34 @@
+const isValid =()=>{
+    let details ={
+        name : document.querySelector('#name').value,
+        email:document.querySelector('#email').value,
+        password:document.querySelector('#email').value,
+
+    }
+    console.log(details.name)
+
+    if (details.name || details.email || details.password === ""){
+        document.querySelector('#name-error').textContent = `error`
+        return false;
+
+    }
+}
+
+
+const validate =(event)=>{
+    event.preventDefault();
+    const check = isValid();
+    if(check){
+        return true;
+
+        // clear form
+        
+        document.querySelector('#name-error').textContent = ""
+
+        console.log('done')
+    }
+
+}
+
+
+document.querySelector('#form').addEventListener('submit',validate)
